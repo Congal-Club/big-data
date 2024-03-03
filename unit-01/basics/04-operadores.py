3d24d96b2e845936c02309f2272bd2d2262549b7
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-03-02 21:28:12 -0600

    Add 04 python file

---
+++ unit-01/basics/04-operadores.py
@@ -0,0 +1,28 @@
+'''
+# Operadores
+'''
+
+x = 10
+y = 20
+z = 15
+
+print("-----Operadores aritméticos-----")
+print(x + y)
+print(x - z)
+print(y / z)
+print(y * z)
+print(z % x)
+print(y ** x)
+
+print("-----Operadores relacionales-----")
+print(x < z)
+print(x > z)
+print(y >= x)
+print(z <= y)
+print(x == 11)
+print(z != 12)
+
+print("-----Operadores lógicos-----")
+print("El resultado es: ",z > 10 and z < 20)
+print("El valor es: ", x >= 10 or x < 5 )
+print("El valor es: ", not(x >= 10))
