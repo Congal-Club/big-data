e8034611104ced349b9865bdecda4239e619df24
Cesar <cesarvillalobosolmos.01@gmail.com>
2024-03-02 21:28:01 -0600

    Add 03 python file

---
+++ unit-01/basics/03-entrada-datos.py
@@ -0,0 +1,19 @@
+'''
+Las tres comillas son para Definir un bloque de texto.
+El símbolo # es para escribir un comentario SOLO en una línea de texto.
+'''
+
+# Entrada de datos desde el teclado.
+
+'''
+La función input(), sirve para capturar la entra de datos desde el tecldado.
+'''
+
+print("Ingresa tu nombre: ")
+nombre = input()
+print("Ingresa tu edad: ")
+edad = input()
+edad = int(edad)
+
+print("Tu nombre es: ", nombre)
+print("Tu edad es: ", edad)
